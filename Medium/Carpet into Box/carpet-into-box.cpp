c20794c52eb35a530da1347985683d6626dd6991
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-01-19 18:52:49 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Carpet into Box/carpet-into-box.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+//User function Template for C++
+
+class Solution{
+    public:
+    int helper(int A, int B, int C, int D){
+         int count=0;
+        while(A>C){
+            A=A/2;
+            count++;
+        }
+        while(B>D){
+            B=B/2;
+            count++;
+        }
+        return count;
+    }
+    
+    int carpetBox(int A, int B, int C, int D){
+        //code here
+       
+     return min(helper(A,B,C,D),helper(A,B,D,C));
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int A,B,C,D;
+        cin>>A>>B>>C>>D;
+        
+        Solution ob;
+
+        int ans = ob.carpetBox(A,B,C,D);
+        
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
