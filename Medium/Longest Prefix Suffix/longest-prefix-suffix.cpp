df0fe47c0ca0907143039e989b8d1f73a0fc0309
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-02-02 19:09:24 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Prefix Suffix/longest-prefix-suffix.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial template for C++ 
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+//User function template for C++
+
+class Solution{
+  public:		
+	int lps(string s) {
+	    // Your code goes here
+	    int i=1;
+	    int j=0;
+	    while(i<s.length()){
+	        if(s[i]==s[j]){
+	            i++;j++;
+	        }
+	        else{
+	            i=i-j+1;
+	            j=0;
+	        }
+	    }
+	    return j;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string str;
+   		cin >> str;
+
+   		Solution ob;
+
+   		cout << ob.lps(str) << "\n";
+   	}
+
+    return 0;
+}
+
+// } Driver Code Ends
