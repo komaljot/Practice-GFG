6152a1ad0874161ebbdd23058aec7655ecb95699
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-02-09 19:40:28 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Recursively remove all adjacent duplicates/recursively-remove-all-adjacent-duplicates.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    string rremove(string s){
+        // code here
+        string ans;
+        int i=0;
+        int n=s.length();
+        while(s[i]){
+            if(s[i]!=s[i+1]){
+                ans+=s[i];
+            }
+            else{
+                while(s[i]==s[i+1])i++;
+            }
+            i++;
+        }
+        if (ans.size()== s.size())
+
+    return ans;
+
+    return rremove(ans);
+
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Solution ob;
+        cout << ob.rremove(s) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
