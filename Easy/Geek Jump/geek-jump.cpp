14c33f159afd3b6961530ae58415e10f363da19f
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-03-01 22:27:10 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Geek Jump/geek-jump.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+   int minimumEnergy(vector<int>& height, int n) {
+        // Code here
+        int prev1=0;
+        int prev2=0;
+        for(int i=1;i<height.size();i++){
+            int fs=prev1+abs(height[i]-height[i-1]);
+            int ss=INT_MAX;
+            if(i>1)
+            ss=prev2+abs(height[i]-height[i-2]);
+            
+            int curri=min(fs,ss);
+            prev2=prev1;
+            prev1=curri;
+           
+        }
+        return prev1;
+   }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.minimumEnergy(arr, N) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
