f5c4f9a7ab72f74acebd7540b8598f0005459c80
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-03-02 09:24:05 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimal Cost/minimal-cost.cpp
+++ Medium/Minimal Cost/minimal-cost.cpp
@@ -6,24 +6,28 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int f(vector<int>& height, int ind, int k,vector<int>&dp){
-      if(ind==0)return 0;
-      if(dp[ind]!=-1)return dp[ind];
-      int mini=INT_MAX;int fe=0;
-      for(int j=1;j<=k;j++){
-          if(ind-j>=0)
-       fe=f(height,ind-j,k,dp)+abs(height[ind]-height[ind-j]);
-      mini=min(mini,fe);
-          
+  int f(vector<int>& height, int n, int k,vector<int>&dp){
+      //tabulation
+      dp[0]=0;
+      for(int i=1;i<n;i++){
+          int mini=INT_MAX;int t=0;
+          for(int j=1;j<=k;j++){
+              if(i-j>=0) 
+              t=dp[i-j]+abs(height[i]-height[i-j]);
+              mini=min(mini,t);
+          }
+          dp[i]=mini;
       }
-      return dp[ind]=mini;
-     
+      return dp[n-1];
+      
       
   }
+  
+  
     int minimizeCost(vector<int>& height, int n, int k) {
         // Code here
         vector<int>dp(n,-1);
-        return f(height,n-1,k,dp);
+        return f(height,n,k,dp);
     }
 };
 
