c42cd58f76dd9061436b1b557ed8981a5c24692f
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-03-04 16:51:58 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find Missing And Repeating/find-missing-and-repeating.cpp
+++ Medium/Find Missing And Repeating/find-missing-and-repeating.cpp
@@ -11,32 +11,32 @@ public:
         int re=-1;
         int me=n+1;
         int *ans=new int[2];
-        // //for finding repeating number
-        // for(int i=0;i<n;i++){
-        //     if(arr[abs(arr[i])-1]<0){
-        //         re=abs(arr[abs(arr[i])-1]);
-        //     }
-        //     arr[abs(arr[i])-1]=-arr[abs(arr[i])-1];
-            
-        // }
-        // for(int i=0;i<n;i++){
-        //     if(arr[i]>0)me=i+1;
-        // }
-        // ans[0]=re;ans[1]=me;
-        // return ans;
-        
-        unordered_map<int,int>mp;
+        //for finding repeating number
         for(int i=0;i<n;i++){
-            mp[arr[i]]++;
+            if(arr[abs(arr[i])-1]<0){
+                re=abs(arr[i]);
+            }
+           else  arr[abs(arr[i])-1]=-arr[abs(arr[i])-1];
             
         }
-        for(int i=0;i<=n;i++){
-            if(mp[i]==2)re=i;
-            if(mp[i]==0)me=i;
-            
+        for(int i=0;i<n;i++){
+            if(arr[i]>=0)me=i+1;
         }
         ans[0]=re;ans[1]=me;
         return ans;
+        
+        // unordered_map<int,int>mp;
+        // for(int i=0;i<n;i++){
+        //     mp[arr[i]]++;
+            
+        // }
+        // for(int i=0;i<=n;i++){
+        //     if(mp[i]==2)re=i;
+        //     if(mp[i]==0)me=i;
+            
+        // }
+        // ans[0]=re;ans[1]=me;
+        // return ans;
     }
 };
 
