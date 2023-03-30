d3e88e7f6056785886274fc8c65e588eaf2be62d
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-03-31 00:09:09 +0530

    Updated Solution - GfG to GitHub

---
+++ Medium/Subset Sums/subset-sums.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+
+void helper(int i,vector<int> arr, int N,vector<int>&ans,int s){
+    if(i==N){
+        ans.push_back(s);
+        return;
+    }
+    
+    helper(i+1,arr,N,ans,s+arr[i]);
+    helper(i+1,arr,N,ans,s);
+    
+    
+    
+}
+
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        // Write Your Code here
+        vector<int>ans;
+        int s=0;
+        helper(0,arr,N,ans,s);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0 ; i < N ; i++){
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<int> ans = ob.subsetSums(arr,N);
+        sort(ans.begin(),ans.end());
+        for(auto sum : ans){
+            cout<< sum<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
