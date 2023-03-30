8a679cff829b7355362118ea8fce1255130d6f1c
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-03-30 22:15:23 +0530

    Updated Solution - GfG to GitHub

--- Easy/Power Set/power-set.cpp
+++ Easy/Power Set/power-set.cpp
@@ -5,36 +5,31 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 	public:
-	void solve(string str,string output,int index,vector<string>& ans){
-
-        //Base Case
-
-        if(index>=str.length()){
-            if(output.length()>0){
-                ans.push_back(output);
-
-            }
-           return;
-        }
-        //exclude
-        solve(str,output,index+1,ans);
-        //include
-        char element=str[index];
-        output.push_back(element);
-        solve(str,output,index+1,ans);
-    }
-
- public:
-
- vector<string> AllPossibleStrings(string s){
-
-     vector<string>ans;
-     string output;
-     int index=0;
-     solve(s,output,index,ans);
-     sort(ans.begin(),ans.end());
-     return ans;
- }
+	
+	void helper(int i,int n,string s,string t,vector<string>&ans){
+	    if(i>=s.length()){
+	        if(t.length()>0){
+	            ans.push_back(t);
+	           
+	        }
+	         return;
+	    }
+	    //not pick
+	    helper(i+1,n,s,t,ans);
+	    //pick;
+	    t.push_back(s[i]);
+	    helper(i+1,n,s,t,ans);
+	    
+	}
+	
+		vector<string> AllPossibleStrings(string s){
+		    // Code here
+		    vector<string>ans;
+		    string t;
+		    helper(0,s.length(),s,t,ans);
+		   sort(ans.begin(),ans.end());
+		    return ans;
+		}
 };
 
 //{ Driver Code Starts.
