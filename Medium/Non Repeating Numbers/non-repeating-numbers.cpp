3d82ae96c7bbc935ed6f36d09e367c0dc91dfb18
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-05-24 00:34:05 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Non Repeating Numbers/non-repeating-numbers.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+        // Code here.
+        int n=nums.size();
+        vector<int>ans;
+        //to find righmost significant bit
+        int x=0;
+        for(int i=0;i<n;i++){
+            x=x^nums[i];
+         }
+         
+        int rmsbt=x&~(x-1);
+        
+        int p=0;int q=0;
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]&rmsbt)p=p^nums[i];
+            else q=q^nums[i];
+        }
+        ans.push_back(p);
+        ans.push_back(q);
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(2 * n + 2);
+    	for(int i = 0; i < 2 * n + 2; i++)
+    		cin >> v[i];
+    	Solution ob;
+    	vector<int > ans = ob.singleNumber(v);
+    	for(auto i: ans)
+    		cout << i << " ";
+    	cout << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
