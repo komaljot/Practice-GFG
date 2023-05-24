3bf7004cfa91021e8a35267122499f1926f5bf0e
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-05-24 15:01:34 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find position of set bit/find-position-of-set-bit.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findPosition(int N) {
+        // code here
+        if(N==0)return -1;
+        if((N&(N-1))==0){
+            int pos=0;
+            while(N){
+                // if(N&0)return pos;
+                pos++;
+                N=N>>1;
+            }
+            return pos;
+        }
+        else return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+
+        cin>>N;
+
+        Solution ob;
+        cout << ob.findPosition(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
