05db9cfbccf18fbaf4520070b2816d10b2520599
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-05-30 22:29:29 +0530

    Updated Solution - GfG to GitHub

--- Basic/Find position of set bit/find-position-of-set-bit.cpp
+++ Basic/Find position of set bit/find-position-of-set-bit.cpp
@@ -7,17 +7,26 @@ class Solution {
   public:
     int findPosition(int N) {
         // code here
+        // if(N==0)retur
         if(N==0)return -1;
-        if((N&(N-1))==0){
-            int pos=0;
-            while(N){
-                // if(N&0)return pos;
-                pos++;
-                N=N>>1;
+        if(N==1)return 1;
+        // int pos=1;
+        // while(N>0){
+        //     if(N&1)return pos;
+        //     N=N>>1;
+        //     pos++;
+            
+        // }
+        
+        int p=0;
+        while(N>=pow(2,p)){
+            if(N==pow(2,p)){
+                return p+1;
             }
-            return pos;
+            p++;
+            
         }
-        else return -1;
+        return -1;
     }
 };
 
