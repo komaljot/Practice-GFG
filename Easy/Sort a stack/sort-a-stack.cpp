2023021862f77b80bf6e7c206b289f224058e29f
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-06-16 16:14:07 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sort a stack/sort-a-stack.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}
+// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+
+void sortedInsert(stack<int>&s,int x){
+    if(s.empty() or (!s.empty() and s.top()<x)){
+        s.push(x);return;
+        
+    }
+    int ele=s.top();
+    s.pop();
+    sortedInsert(s,x);
+    s.push(ele);
+    
+    
+}
+void SortedStack :: sort()
+{
+   //Your code here
+   
+   if(s.empty())return;
+   
+   int ele=s.top();
+   s.pop();
+   sort();
+   
+   sortedInsert(s,ele);
+   
+}
