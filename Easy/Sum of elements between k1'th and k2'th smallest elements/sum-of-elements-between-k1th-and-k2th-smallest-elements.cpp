5d223e6a553699859e863609b96181a0154b987d
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-07-27 21:37:29 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sum of elements between k1'th and k2'th smallest elements/sum-of-elements-between-k1th-and-k2th-smallest-elements.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long sumBetweenTwoKth( long long A[], long long N, long long K1, long long K2)
+    {
+        // Your code goes here
+        sort(A,A+N);
+        int a=K1-1;a++;
+        int b=K2-1;
+        long long ans=0;
+        while(a<b){
+            
+            ans+=A[a];
+            a++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n, k;
+        cin>>n;
+        long long a[n+5];
+        for(int i =0;i<n;i++)
+            cin >> a[i];
+        
+        long long k1, k2;
+        cin >> k1 >> k2;
+        Solution ob;
+        cout << ob.sumBetweenTwoKth(a, n, k1, k2) << endl;
+        
+    }
+	return 0;
+}
+
+// } Driver Code Ends
