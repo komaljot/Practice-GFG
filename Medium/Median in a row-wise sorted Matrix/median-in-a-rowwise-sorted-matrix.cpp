3ad050e490ff0ffc223a4f0079748550954728db
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-08-08 19:16:22 +0530

    Updated Solution - GfG to GitHub

--- Medium/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
+++ Medium/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
@@ -11,33 +11,34 @@ using namespace std;
 class Solution{   
 public:
 
-int findcountlessthaneqto(vector<int>row,int mid){
+int lessthaneqto(vector<int>&R,int mid){
+    int cnt=0;
     int low=0;
-    int high=row.size()-1;
+    int high=R.size()-1;
     while(low<=high){
         int m=(low+high)/2;
-        if(row[m]<=mid)low=m+1;
+        if(R[m]<=mid)low=m+1;
         else high=m-1;
     }
     return low;
     
 }
 
-
     int median(vector<vector<int>> &matrix, int R, int C){
-        // code here    
+        // code here   
         int low=0;
         int high=1e9;
-        int n=matrix.size();
-        int m=matrix[0].size();
         while(low<=high){
             int mid=(low+high)/2;
             int cnt=0;
-            for(int i=0;i<n;i++){
-            cnt+=findcountlessthaneqto(matrix[i],mid);
+            for(int i=0;i<R;i++){
+                
+                cnt+=lessthaneqto(matrix[i],mid);
             }
-            if(cnt>(m*n)/2)high=mid-1;
-             else low=mid+1;
+            if(cnt>(R*C)/2)high=mid-1;
+            else {
+                low=mid+1;
+            }
             
         }
         return low;
