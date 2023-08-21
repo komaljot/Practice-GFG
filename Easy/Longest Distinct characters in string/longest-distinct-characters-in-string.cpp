48a4bdd9dfd87f5d61274ccfa9162eb3a6f36820
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-08-21 11:32:15 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Longest Distinct characters in string/longest-distinct-characters-in-string.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+int longestSubstrDistinctChars (string S);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string S; cin >> S;
+
+        cout << longestSubstrDistinctChars (S) << endl;
+    }
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+int longestSubstrDistinctChars (string S)
+{
+    // your code here
+    unordered_map<char,int>mp;
+    
+    int i=0;
+    int j=0;
+    int ans=0;
+    while(j<S.length()){
+        mp[S[j]]++;
+        if(mp.size()==(j-i+1)){
+           ans=max(ans,(j-i+1));
+           j++;
+            
+        }
+        else if(mp.size()<(j-i+1)){
+            while(mp.size()<(j-i+1)){
+                mp[S[i]]--;
+                if(mp[S[i]]==0)mp.erase(S[i]);
+                i++;
+            }
+            j++;
+        }
+      
+        
+    }
+      return ans;
+}
