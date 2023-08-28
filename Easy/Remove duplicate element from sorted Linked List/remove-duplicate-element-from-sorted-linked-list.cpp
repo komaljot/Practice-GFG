f5d5c91d8f5a72815c869eb66f17d8a670f97838
Komaljot Kaur <78251379+komaljot@users.noreply.github.com>
2023-08-28 15:19:11 +0530

    Updated Solution - GfG to GitHub

--- Easy/Remove duplicate element from sorted Linked List/remove-duplicate-element-from-sorted-linked-list.cpp
+++ Easy/Remove duplicate element from sorted Linked List/remove-duplicate-element-from-sorted-linked-list.cpp
@@ -69,12 +69,14 @@ struct Node {
 Node *removeDuplicates(Node *head)
 {
  // your code goes here
- Node* temp=head;
- while(temp->next!=NULL){
-     if(temp->data==temp->next->data){
-         temp->next=temp->next->next;
-     }
-     else temp=temp->next;
- }
+Node* temp=head;
+while(temp->next!=NULL){
+    if(temp->data==temp->next->data){
+        temp->next=temp->next->next;
+    }
+    else temp=temp->next;
+}
+
  return head;
+ 
 }
